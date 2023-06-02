b0eed9ee89d3da12eb2feeaa720b2b485d64cd4d
Cynthia Nelson <cynthia.v.nelson7@gmail.com>
2023-06-02 13:33:44 -0500

    adding lecture notes and exercise for form binding and services

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -8,14 +8,13 @@ import com.codeup.codeupspringblog.repositories.CategoryRepository;
 import com.codeup.codeupspringblog.repositories.CommentRepository;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.Collections;
+import java.util.*;
 
 @Controller
 public class PostController {
@@ -25,11 +24,14 @@ public class PostController {
     private CommentRepository commentsDao;
     private CategoryRepository categoriesDao;
 
-    public PostController(PostRepository postsDao, UserRepository usersDao, CommentRepository commentsDao, CategoryRepository categoriesDao) {
+    private final EmailService emailService;
+
+    public PostController(PostRepository postsDao, UserRepository usersDao, CommentRepository commentsDao, CategoryRepository categoriesDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.usersDao = usersDao;
         this.commentsDao = commentsDao;
         this.categoriesDao = categoriesDao;
+        this.emailService = emailService;
     }
 
     public Set<Category> makeCategorySet(String categoriesCsl){
@@ -47,6 +49,7 @@ public class PostController {
     @GetMapping("/posts")
     public String allPosts(Model model) {
         List<Post> posts = postsDao.findAll();
+        Collections.reverse(posts);
         model.addAttribute("posts", posts);
         return "posts/index";
     }
@@ -72,8 +75,24 @@ public class PostController {
         return "posts/create";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String editForm(@PathVariable long id, Model model){
+        Post post = postsDao.findById(id);
+        model.addAttribute("post", post);
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String submitEditForm(@PathVariable long id, @ModelAttribute Post post){
+        User user = usersDao.findUserById(1l);
+        post.setUser(user);
+        post.setId(id);
+        postsDao.save(post);
+        return "redirect:/posts/" + id;
+    }
+
     @PostMapping("/posts/create")
-    public String submitForm(@ModelAttribute("post") Post post, @RequestParam("categories") String categories) {
+    public String submitForm(@ModelAttribute Post post, @RequestParam("categories") String categories) {
         User user = usersDao.findUserById(1L);
         post.setUser(user);
         Set<Category> categorySet = makeCategorySet(categories);
